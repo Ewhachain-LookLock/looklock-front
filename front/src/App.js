c76f118973e1bea182469dd1c18fba33828b2d3e
JAE YEON KIM <65452447+jaethewiederholen@users.noreply.github.com>
2022-08-20 03:05:32 +0900

    connect sequence wallet

--- front/src/App.js
+++ front/src/App.js
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
+// sequence wallet
+import { ethers } from 'ethers'
+import Web3Modal from '@0xsequence/web3modal'
+import { sequence } from '0xsequence';
+import { configureLogger } from '@0xsequence/utils'
+
 import NavBar from "./component/NavBar";
 import Main from "./pages/Main";
 import NoPage from "./pages/NoPage";
@@ -17,61 +23,151 @@ import Admin from './pages/Admin';
 import Register from './pages/Register';
 import Memo from "./components/Memo";
 
+configureLogger({ logLevel: 'DEBUG' })
+
+let providerOptions = {
+}
+
+if (!window?.ethereum?.isSequence) {
+  providerOptions = {
+    sequence: {
+      package: sequence,
+      options: {
+        appName: 'LookLock',
+        defaultNetwork: 'polygon'
+      }
+    }
+  }
+}
+
+const web3Modal = new Web3Modal({
+  providerOptions,
+  cacheProvider: true
+})
+
+
+
 export default function App() {
+  const [provider, setProvider] = useState(null);
   const [currentAccount, setCurrentAccount] = useState('');
   const [correctNetwork, setCorrectNetwork] = useState(false);
 
-  const checkIfWalletIsConnected = async () => {
-      const {ethereum} = window;
-      if (ethereum) {
-          console.log('Got the ethereum object: ', ethereum);
-      }else {
-          console.log('No Wallet found. Connect Wallet');
-      }
+  useEffect(() => {
+    // if (web3Modal.cachedProvider) {
+    //   connectWallet()
+    // }
+    checkCorrectNetwork();
 
-      const accounts = await ethereum.request({method: "eth_accounts"});
+  }, [currentAccount])
 
-      if (accounts.length !== 0) {
-          console.log('Found authorized Account: ', accounts[0]);
-          setCurrentAccount(accounts[0]);
-      }else {
-          console.log('No authorized account found');
-      }
+  const connectWeb3Modal = async () => {
+    if (web3Modal.cachedProvider) {
+      web3Modal.clearCachedProvider()
+    }
+    connectWallet()
   };
 
   const connectWallet = async () => {
-      try {
-        const {ethereum} = window;
-  
-        if(!ethereum) {
-          console.log('Metamask not detected');
-          return;
-        }
-        let chainId = await ethereum.request({method:'eth_chainId'})
-        
-        const address = await ethereum.enable();
-        await console.log('address : ', address);
-        
-        const hardhatChainId = '0x539'
-        if (chainId !== hardhatChainId) {
-          alert('You are not connected to the Hardhat Testnet!');
-          return;
-        }
-      
-        console.log('Found account', address[0]);
-        setCurrentAccount(address[0]);
-      } catch(error) {
-        console.log('Error connecting to metamask', error)
-      }
+    const wallet = await web3Modal.connect()
+    console.log('wallet : ' , wallet);
+    const provider = new ethers.providers.Web3Provider(wallet)
+
+    if (wallet.sequence) {
+      ;(provider).sequence = wallet.sequence
+    }
+
+    setProvider(provider);
+    console.log([provider]);
+    getAccounts(provider);
+
   }
 
+  const disconnectWeb3Modal = async () => {
+    web3Modal.clearCachedProvider();
+    console.log("clicked")
+    if (provider && (provider).sequence) {
+      const wallet = (provider).sequence;
+      wallet.disconnect()
+    }
+
+    setProvider(null);
+    setCurrentAccount('');
+    
+    
+  }
+
+  const getChainID = async () => {
+    if (provider) {
+      const signer = provider?.getSigner();
+      const chainId = await signer.getChainId()
+      console.log('signer.getChainId()', chainId, typeof(chainId));
+      return chainId;
+    }
+    else return
+  }
+
+  const getAccounts = async (provider) => {
+    const signer = provider.getSigner();
+    const account = await signer.getAddress();
+    console.log('getAddress():', account);
+    console.log('ci : ' , await signer.getChainId());
+    setCurrentAccount(account);
+  }
+
+
+  // const checkIfWalletIsConnected = async () => {
+  //     const {ethereum} = window;
+  //     if (ethereum) {
+  //         console.log('Got the ethereum object: ', ethereum);
+  //     }else {
+  //         console.log('No Wallet found. Connect Wallet');
+  //     }
+
+  //     const accounts = await ethereum.request({method: "eth_accounts"});
+
+  //     if (accounts.length !== 0) {
+  //         console.log('Found authorized Account: ', accounts[0]);
+  //         setCurrentAccount(accounts[0]);
+  //     }else {
+  //         console.log('No authorized account found');
+  //     }
+  // };
+
+  // const connectWallet = async () => {
+  //     try {
+  //       const {ethereum} = window;
+  
+  //       if(!ethereum) {
+  //         console.log('Metamask not detected');
+  //         return;
+  //       }
+  //       let chainId = await ethereum.request({method:'eth_chainId'})
+        
+  //       const address = await ethereum.enable();
+  //       await console.log('address : ', address);
+        
+  //       const hardhatChainId = '0x539'
+  //       if (chainId !== hardhatChainId) {
+  //         alert('You are not connected to the Hardhat Testnet!');
+  //         return;
+  //       }
+      
+  //       console.log('Found account', address[0]);
+  //       setCurrentAccount(address[0]);
+  //     } catch(error) {
+  //       console.log('Error connecting to metamask', error)
+  //     }
+  // }
+
   const changeNetwork = async() => {
-      const chainId = '0x539'
-      if (window.ethereum.networkVersion !== chainId) {
+      const localhost = '0x539'
+      const rinkeby = '0x4'
+      const polygon = '0x89'
+      if (![localhost, rinkeby, polygon].includes(window.ethereum.networkVersion)) {
           try {
             await window.ethereum.request({
               method: 'wallet_switchEthereumChain',
-              params: [{ chainId: chainId }]
+              params: [{ chainId: localhost }]
             });
           } catch (err) {
               // This error code indicates that the chain has not been added to MetaMask
@@ -81,7 +177,7 @@ export default function App() {
                 params: [
                   {
                     chainName: 'Localhost',
-                    chainId: chainId,
+                    chainId: localhost,
                     rpcUrls: ['http://localhost:8545/']
                   }
                 ]
@@ -92,11 +188,8 @@ export default function App() {
   }
 
     const checkCorrectNetwork = async () => {
-        const {ethereum} = window;
-        let chainId = await ethereum.request({method : 'eth_chainId'})
-        console.log('Conneted to chian' + chainId);
-    
-        const hardhatChainId = '0x539'
+        const chainId = await getChainID();
+        const hardhatChainId = 1337
     
         if (chainId !== hardhatChainId) {
           setCorrectNetwork(false)
@@ -104,11 +197,6 @@ export default function App() {
           setCorrectNetwork(true)
         }
     }
-
-    useEffect(() => {
-      checkIfWalletIsConnected();
-      checkCorrectNetwork();
-    }, [currentAccount]);
 
   return (
     <div className="App">
@@ -118,8 +206,9 @@ export default function App() {
               <NavBar 
                 currentAccount={currentAccount}
                 correctNetwork= {correctNetwork}
-                connectWallet = {connectWallet}
+                connectWallet = {connectWeb3Modal}
                 changeNetwork = {changeNetwork}
+                disconnectWeb3Modal = {disconnectWeb3Modal}
               ></NavBar>}>
             <Route index element={<Main />} />
             <Route path="project" element={<Project />} />
